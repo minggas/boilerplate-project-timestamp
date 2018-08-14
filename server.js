ec55a7493c3c3660caaf0719b197be4806770213
Fabricio Asfora Lira <32206134+minggas@users.noreply.github.com>
2018-08-14 01:56:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 // where your node app starts
 
 // init project
+var moment = require('moment');
 var express = require('express');
+var parser = require('./scripts');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -25,16 +27,24 @@ app.get("/api/hello", function (req, res) {
 });
 
 //time API endpoint
-app.get("/api/timestamp/:date", function(req, res){
-  var date = Date.parse(req.params.date);
-  var natural = new Date(date).toLocaleString('en-GB',{year: 'numeric', month: 'long', day: 'numeric' });
-  if(date){
-    res.json({unix: date, natural: natural});
-  }else{
-    res.json({unix: null, natural: "Invalid Date"});
-  }  
+app.get("/api/timestamp", function(req, res){
+  var date = moment();
+  return res.json({unix: date.unix(), natural: moment(date).format("dddd, MMMM Do YYYY, h:mm:ss a")});  
 });
 
+app.get("/api/timestamp/:date", function(req, res){
+  var date = new Date(parser.parseDate(req.params.date));
+  return res.json({unix: date.getTime(), natural: date.toUTCString()});  
+});
+
+app.get('api/shorten/new/:url',function(req,res){
+  var url=req.params.url;
+  //var isUrl = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  //if(isUrl.test(url)){
+    res.json({"original url":url})
+ // } else {
+  //  res.json("Please enter a valid url")}
+});
 
 
 // listen for requests :)
